11299db8ac601162d560a4a23577c3a341a8f30d
RizkyMaulana310807 <120023520+RizkyMaulana310807@users.noreply.github.com>
2023-11-22 08:19:22 +0700

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,36 @@
+public class dowhile {
+    public static void main(String[] args) {
+        //Perulangan menggunakan do while
+        System.out.println("|| Do While Perulangan ||");
+        int i = 1;
+        do {
+            System.out.print(i + ", ");
+            i += 2;
+        } while (i <= 100);
+        System.out.println("");
+        System.out.println("|| Do While perulangan ||");
+        
+        int j = 2;
+        do{
+            System.out.print(j + ", ");
+            j += 2;
+        } while (j <= 100);
+
+        //Perulangan menggunakan While
+        System.out.println("");
+        System.out.println("|| While perulangan ||");
+        int k = 1;
+        while (k <= 100) {
+            System.out.print(k + ", ");
+            k += 2;
+        }
+        System.out.println("");
+        System.out.println("|| While perulangan ||");
+        int l = 2;
+        while (l <= 100) {
+            System.out.print(l + ", ");
+            l += 2;
+        }
+        System.out.println("");
+    }
+}
